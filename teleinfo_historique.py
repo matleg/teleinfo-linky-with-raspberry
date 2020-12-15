a065fbd94c34052a92e6e0adbfef0ec52c37dfb6
Boris Tassou <boris.tassou@easylience.com>
2020-12-15 17:14:38 +0100

    pep 8 teleinfo_historique.py, rated at 9.87/10

--- teleinfo_historique.py
+++ teleinfo_historique.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # __author__ = "Sébastien Reuiller"
 # __licence__ = "Apache License 2.0"
+"""Send teleinfo history to influxdb."""
 
 # Python 3, prerequis : pip install pySerial influxdb
 #
@@ -21,77 +22,78 @@
 #  'PTEC': 'HP..'            # Période tarifaire en cours
 # }
 
-
-import serial
 import logging
 import time
-import requests
 from datetime import datetime
+import requests
+import serial
 from influxdb import InfluxDBClient
 
 # clés téléinfo
-int_measure_keys = ['BASE','IMAX', 'HCHC', 'IINST', 'PAPP', 'ISOUSC', 'ADCO', 'HCHP']
-no_checksum = ['MOTDETAT']
+INT_MEASURE_KEYS = ['BASE', 'IMAX', 'HCHC', 'IINST', 'PAPP', 'ISOUSC', 'ADCO', 'HCHP']
+NO_CHECKSUM = ['MOTDETAT']
 
 # création du logguer
-logging.basicConfig(filename='/var/log/teleinfo/releve.log', level=logging.INFO, format='%(asctime)s %(message)s')
+logging.basicConfig(filename='/var/log/teleinfo/releve.log',
+                    level=logging.INFO, format='%(asctime)s %(message)s')
 logging.info("Teleinfo starting..")
 
 # connexion a la base de données InfluxDB
-client = InfluxDBClient('localhost', 8086)
-db = "teleinfo"
-connected = False
-while not connected:
+CLIENT = InfluxDBClient('localhost', 8086)
+DB = "teleinfo"
+CONNECTED = False
+while not CONNECTED:
     try:
-        logging.info("Database %s exists?" % db)
-        if not {'name': db} in client.get_list_database():
-            logging.info("Database %s creation.." % db)
-            client.create_database(db)
-            logging.info("Database %s created!" % db)
-        client.switch_database(db)
-        logging.info("Connected to %s!" % db)
+        logging.info("Database %s exists?", DB)
+        if {'name': DB} not in CLIENT.get_list_database():
+            logging.info("Database %s creation..", DB)
+            CLIENT.create_database(DB)
+            logging.info("Database %s created!", DB)
+        CLIENT.switch_database(DB)
+        logging.info("Connected to %s!", DB)
     except requests.exceptions.ConnectionError:
         logging.info('InfluxDB is not reachable. Waiting 5 seconds to retry.')
         time.sleep(5)
     else:
-        connected = True
+        CONNECTED = True
 
 
-def add_measures(measures, time_measure):
+def add_measures(measures):
+    """Add measures to array."""
     points = []
     for measure, value in measures.items():
         point = {
-                    "measurement": measure,
-                    "tags": {
-                        # identification de la sonde et du compteur
-                        "host": "raspberry",
-                        "region": "linky"
-                    },
-                    "time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                    "fields": {
-                        "value": value
-                    }
+            "measurement": measure,
+            "tags": {
+                # identification de la sonde et du compteur
+                "host": "raspberry",
+                "region": "linky"
+            },
+            "time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "fields": {
+                "value": value
                 }
+            }
         points.append(point)
 
-    client.write_points(points)
+    CLIENT.write_points(points)
 
 
-def verif_checksum(data,checksum):
+def verif_checksum(data, checksum):
+    """Check data checksum."""
     data_unicode = 0
     for caractere in data:
-            data_unicode += ord(caractere)
+        data_unicode += ord(caractere)
     sum_unicode = (data_unicode & 63) + 32
-    sum = chr(sum_unicode)
-    if (checksum == sum):
-        return True
-    else:
-        return False
+    sum_chain = chr(sum_unicode)
+    return bool(checksum == sum_chain)
 
-def analyse_ADCO(ADCO):
+# def analyse_ADCO(ADCO):
 
 def main():
-    with serial.Serial(port='/dev/ttyS0', baudrate=1200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+    """Main function to read teleinfo."""
+    with serial.Serial(port='/dev/ttyS0', baudrate=1200, parity=serial.PARITY_NONE,
+                       stopbits=serial.STOPBITS_ONE,
                        bytesize=serial.SEVENBITS, timeout=1) as ser:
 
         logging.info("Teleinfo is reading on /dev/ttyS0..")
@@ -109,43 +111,45 @@ def main():
         while True:
             line_str = line.decode("utf-8")
             logging.debug(line)
-            ar = line_str.split(" ") # separation sur espace /!\ attention le caractere de controle 0x32 est un espace aussi
+            # separation sur espace /!\ attention le caractere de controle 0x32 est un espace aussi
+            ar_split = line_str.split(" ")
             # preparation données pour verification checksum
-            data =  ar[0] + " " + ar[1]
-            checksum = (line_str.replace('\x03\x02',''))[-3:-2]  # supprimer les retours charriot et saut de ligne puis selectionne le caractere de controle en partant de la fin
-            verif_checksum(data,checksum)
-
+            data = ar_split[0] + " " + ar_split[1]
+            # supprimer les retours charriot et saut de ligne puis
+            # selectionne le caractere de controle en partant de la fin
+            checksum = (line_str.replace('\x03\x02', ''))[-3:-2]
+            return_checksum = verif_checksum(data, checksum)
 
             try:
-                key = ar[0]
-                if key in int_measure_keys :  # typer les valeurs numériques en "integer"
-                    value = int(ar[1])
+                key = ar_split[0]
+                if key in INT_MEASURE_KEYS:  # typer les valeurs numériques en "integer"
+                    value = int(ar_split[1])
                 else:
-                    value = ar[1]   # typer les autres valeurs en "string"
-               
+                    value = ar_split[1]   # typer les autres valeurs en "string"
+
                 trame[key] = value   # creation du champ pour la trame en cours
-                
-                if b'\x03' in line:  # si caractère de fin dans la ligne, on insère la trame dans influx
-                    del trame['ADCO']  # adresse du compteur : confidentiel!
+
+                # si caractère de fin dans la ligne, on insère la trame dans influx
+                if b'\x03' in line:
+                    if trame['ADCO']:
+                        del trame['ADCO']  # adresse du compteur : confidentiel!
                     time_measure = time.time()
 
                     # insertion dans influxdb
-                    if (verif_checksum) : 
-                        add_measures(trame, time_measure)
+                    if return_checksum:
+                        add_measures(trame)
 
                     # ajout timestamp pour debugger
                     trame["timestamp"] = int(time_measure)
                     logging.debug(trame)
 
                     trame = dict()  # on repart sur une nouvelle trame
-            except Exception as e:
-                logging.error("Exception : %s" % e, exc_info=True)
-                logging.error("%s %s" % (key,value))
+            except Exception as error:
+                logging.error("Exception : %s", error, exc_info=True)
+                logging.error("%s %s", key, value)
             line = ser.readline()
 
 
 if __name__ == '__main__':
-    if connected:
+    if CONNECTED:
         main()
-
-
